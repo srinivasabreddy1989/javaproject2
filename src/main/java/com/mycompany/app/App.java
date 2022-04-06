c1f4c22718334084f78942331ee85313bcc7a4fb
srinivasabreddy1989 <srinivasabreddy1989@gmail.com>
2022-04-06 13:34:07 +0530

    web hook config

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! how are you ";
+    private final String message = "Hello World! how are you mine";
 
     public App() {}
 
